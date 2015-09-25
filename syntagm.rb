0632d8d034a7cbe74f22e881d804806ce033e47a
sotomo <programmerchicago@live.com>
2015-09-25 03:34:47 +0000

    added class

--- syntagm.rb
+++ syntagm.rb
@@ -1,6 +1,21 @@
 require 'optparse'
 require 'net/http'
 
+
+class Syntagm
+
+  def initialize(text, type)
+    @text = text
+    @type = type
+  end
+  
+  def wrapup
+      @text=@text.to_s.strip
+  end
+  
+end
+
+
 options = {}
 
 optparse = OptionParser.new do |opts|
@@ -22,29 +37,51 @@ sentence = @sentence
 hashTags = @sentence.scan(/\@\w+/)
 urls = URI.extract(@sentence)
 
-extractions = []
+syntagms = []
 
+phrase = ""
 hashTags.each { |a| 
-  extractions.push(a) 
   sentence.slice! a
-}
-
-urls.each { |u| 
-  extractions.push(u) 
-  sentence.slice! u
-}
-
-syntagm = ""
-sentence.split(' ').each { |w| 
-  if(syntagm.length>@maxchars.to_i)
-    extractions.push(syntagm)
-    syntagm=""
+  if(phrase.length>@maxchars.to_i)
+    syntagm = Syntagm.new(phrase, "H")
+    syntagms.push(syntagm)
+    phrase=""
   end
-  syntagm << w
+  phrase << a << " "
+}
+syntagm = Syntagm.new(phrase, "U")
+syntagms.push(syntagm)
+
+
+phrase = ""
+urls.each { |u| 
+  sentence.slice! u
+  if(phrase.length>@maxchars.to_i)
+    syntagm = Syntagm.new(phrase, "U")
+    syntagms.push(syntagm)
+    phrase=""
+  end
+  phrase << u << " "
+}
+syntagm = Syntagm.new(phrase, "U")
+syntagms.push(syntagm)
+
+phrase = ""
+sentence.split(' ').each { |w| 
+  if(phrase.length>@maxchars.to_i)
+    syntagm = Syntagm.new(phrase, "S")
+    syntagms.push(syntagm)
+    phrase=""
+  end
+  phrase << w << " "
+}
+syntagms.push(syntagm)
+
+syntagms.each { |s|
+    s.wrapup
 }
 
-
-p extractions
+p syntagms
 
 
 
